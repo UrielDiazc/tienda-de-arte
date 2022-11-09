1bcef4cdc945e650f2abeef8ac9a9bd6851a535e
UrielDiazc <112593168+UrielDiazc@users.noreply.github.com>
2022-11-09 09:28:47 -0600

    Add files via upload

---
+++ practica10/java.js
@@ -0,0 +1,18 @@
+function imagen(evento){
+  const Miniatura = document.getElementById('art');
+
+}
+var container = document.querySelectorAll('art')
+
+class Menu {
+constructor() {
+this.botonContenedor = document.querySelector('#menu');
+this.estatus = document.querySelector('#estatus');
+this.buttons = [
+new Boton(this.botonContenedor, 'Marat'),
+new Boton(this.botonContenedor, ''),
+new Boton(this.botonContenedor, 'C')
+];
+}
+}
+const menu = new Menu();
